3a5f88a83ca4d759da29002076c851102a10ee9a
qb1968 <qb1968@aol.com>
2021-08-22 20:30:54 -0400

    build

---
+++ build/precache-manifest.3b422d3db0101651b9ed70865e5bcb12.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bdbcf9bc98c2ab649aa96ef34c89ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "959f7a3a69030c8b37c1",
+    "url": "/static/css/2.316749da.chunk.css"
+  },
+  {
+    "revision": "505bb0d487cee9a1da34",
+    "url": "/static/css/main.e48016bd.chunk.css"
+  },
+  {
+    "revision": "959f7a3a69030c8b37c1",
+    "url": "/static/js/2.7913f076.chunk.js"
+  },
+  {
+    "revision": "a8ac2a586c65f3f8b632a7a6060fd1ba",
+    "url": "/static/js/2.7913f076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505bb0d487cee9a1da34",
+    "url": "/static/js/main.4a58824e.chunk.js"
+  },
+  {
+    "revision": "94d5a7cadcd51f42ad6b",
+    "url": "/static/js/runtime-main.5c3eaa15.js"
+  },
+  {
+    "revision": "ee38c75cb7fb872010a4c1ba1e2d93e0",
+    "url": "/static/media/NewDustin2.ee38c75c.jpg"
+  },
+  {
+    "revision": "64250da840c16199894fd55cf76e9e9a",
+    "url": "/static/media/comics.64250da8.pdf"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
